6996c1a00a27981d231f9c132d52ce6786258925
hsm <hsm15031585502@163.com>
2021-06-29 16:01:10 +0800

    项目初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  lang: 'zh-CN',
+  title: '前端小贺',
+  description: '前端小贺的前端记录',
+  themeConfig: {
+    logo: '/images/logo-small.png',
+    nav: [
+      { text: '首页', link: '/'},
+      { text: '收藏', link: '/cllection/'},
+      { text: '首页', link: '/life/'},
+      { text: 'GitHub', link: 'https://github.com/heshimang'},
+    ]
+  }
+}
